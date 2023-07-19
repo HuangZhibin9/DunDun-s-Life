d5f72d7b3cbd4c587a8435aeaa9c8d0c92e1aac7
WhYBe <5452849@qq.com>
2023-07-19 20:24:38 +0800

    ui

--- DunDun's Life/Assets/Scripts/SaveSystem/Data.cs
+++ DunDun's Life/Assets/Scripts/SaveSystem/Data.cs
@@ -90,7 +90,7 @@ public class Data : MonoBehaviour
     {
         if (scene.name == "FirstScene")
         {
-            GameObject.Find("BeginTimelLine").SetActive(false);
+            //GameObject.Find("BeginTimelLine").SetActive(false);
             dog = GameObject.Find("dog").transform;
             dog.position = _SaveData_for_sceneloaded.Position;
             dog.rotation = _SaveData_for_sceneloaded.Rotation;
